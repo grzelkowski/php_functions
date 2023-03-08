fc1c08528df6043eccc16fdd4a5d44c4daf99318
Pawel <grzelek9@gmail.com>
2023-03-08 17:37:29 +0000

    added function "clean_tags_from_unnecessary_attributes"

---
+++ functions.php
@@ -0,0 +1,8 @@
+<?php
+function clean_tags_from_unnecessary_attributes($string){
+    //remove attributes from tag <> : style="*", font-size="*", trebuchet="*", background-color="*", trebuchet="*", droid="*"
+    $pattern = "/( style| font-size| trebuchet | background-color| trebuchet| droid)[a-zA-Z0-9:;.,\"\-+=()\s]*\"/i";
+    $replacement = '';
+    return preg_replace($pattern, $replacement, $string);
+}
+?>
